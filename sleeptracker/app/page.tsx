d7f620f2830c432a393ff82e04c43abded8ee858
andxrew <134392388+andxrew@users.noreply.github.com>
2024-04-08 22:52:35 +0100

    Inital Template

---
+++ sleeptracker/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex text-blue-500 min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        SLEEP TRACKER START
+      </div>
+    </main>
+  );
+}
